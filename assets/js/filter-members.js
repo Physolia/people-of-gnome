5b597edefa2861dda2b9058578f9e66a55498891
Arijit Kundu <kunduarijit55@gmail.com>
2021-08-03 03:24:30 +0530

    refactor: revert changes done in filter-members.js

--- assets/js/filter-members.js
+++ assets/js/filter-members.js
@@ -1,50 +1,54 @@
-const gnomeMembers = jQuery('.gnome-member');
+const gnomeMembers = Array.from(document.querySelectorAll(".gnome-member"));
 
 let currentMemberPage = 1;
 let currentMemberSearch = '';
 
 const amountOfContributors = gnomeMembers.length - 1;
-const amountofMembersPerPage = 10;
-const amountOfMemberPages = amountOfContributors >= amountofMembersPerPage ? Math.round(amountOfContributors / amountofMembersPerPage) : 1;
+const amountOfMembersPerPage = 10;
+const amountOfMemberPages = amountOfContributors >= amountOfMembersPerPage ? Math.round(amountOfContributors / amountOfMembersPerPage) : 1;
 
 const filterMethod = () => {
-  gnomeMembers.filter(function () {
-    const textSearchResult = jQuery(this).find('.gnome-member-name').text().toLowerCase().indexOf(
-      currentMemberSearch) > -1;
+    gnomeMembers.filter((member) => {
+        const jqueryMember = jQuery(member);
 
-    const currentIndex = parseInt(jQuery(this).attr('data-index'));
+        const textSearchResult = jqueryMember.find('.gnome-member-name').text().toLowerCase().indexOf(
+            currentMemberSearch) > -1;
 
-    const maximumPageNumberResult = currentIndex <= currentMemberPage * amountofMembersPerPage;
+        const currentIndex = parseInt(jqueryMember.attr('data-index'));
 
-    const minimalPageNumberResult = currentMemberPage > 1 ? currentIndex > ((currentMemberPage - 1) * amountofMembersPerPage) : true;
+        const maximumPageNumberResult = currentIndex <= currentMemberPage * amountOfMembersPerPage;
 
-    jQuery(this).toggle(textSearchResult && maximumPageNumberResult && minimalPageNumberResult);
-  });
+        const minimalPageNumberResult = currentMemberPage > 1 ? currentIndex > ((currentMemberPage - 1) * amountOfMembersPerPage) : true;
+
+        jqueryMember.toggle(textSearchResult && maximumPageNumberResult && minimalPageNumberResult);
+    });
 };
 
 const showPage = (pagination) => {
-  const element = jQuery(pagination);
+    const element = jQuery(pagination);
 
-  currentMemberPage = parseInt(element.text());
+    currentMemberPage = parseInt(element.text());
 
-  filterMethod();
+    filterMethod();
 
-  jQuery('.page-item').removeClass('active');
+    jQuery('.page-item').removeClass('active');
 
-  element.addClass('active');
+    element.addClass('active');
 };
 
+
+const paginationMembers = jQuery('.pagination');
+
 for (let i = 1; i <= amountOfMemberPages; i++) {
-  jQuery('.pagination').
-  append(
-    `<li class="page-item" style="cursor: pointer" onClick="showPage(this)"><span class="page-link">${i}</span></li>`
+    paginationMembers.append(
+        `<li class="page-item" style="cursor: pointer" onClick="showPage(this)"><span class="page-link">${i}</span></li>`
     );
 }
 
 jQuery('.gnome-members-filter').on('keyup', function () {
     currentMemberSearch = jQuery(this).val().toLowerCase();
 
-  filterMethod();
+    filterMethod();
 });
 
 jQuery('.page-item').first().addClass('active');
@@ -52,5 +56,5 @@ jQuery('.page-item').first().addClass('active');
 filterMethod();
 
 if (amountOfContributors <= 0) {
-  jQuery('.gnome-members-list').append('<span class="text-muted"><strong>No entries found.</strong></span>');
+    jQuery('.gnome-members-list').append('<span class="text-muted"><strong>No entries found.</strong></span>');
 }
