799fbd9d7b960e020871b2e7a383bcf8267c7e50
Claudio Wunder <cwunder@gnome.org>
2021-08-02 12:39:54 +0200

    fix(scripts): cleaned-up scripts and refactored some of them

--- assets/js/filter-projects.js
+++ assets/js/filter-projects.js
@@ -1,55 +1,74 @@
-const gnomeProjects = jQuery('.gnome-project');
+const gnomeProjects = Array.from(document.querySelectorAll('.gnome-project'));
+
+const filteredGnomeProjects = gnomeProjects.filter((project) => project.querySelector(".project-members").children.length > 0);
 
 let currentProjectPage = 1;
 let currentProjectSearch = '';
 
 const amountOfProjectsPerPage = 10;
 
-const amountOfProjects = gnomeProjects.length - 1;
+const amountOfProjects = filteredGnomeProjects.length - 1;
 const amountOfProjectsPages = amountOfProjects >= amountOfProjectsPerPage ? Math.round(amountOfProjects / amountOfProjectsPerPage) : 1;
 
 const filterProjectMethod = () => {
-  gnomeProjects.filter(function () {
-    const textSearchResultOfMember = jQuery(this).find('.gnome-member-name').text().toLowerCase().indexOf(
-      currentProjectSearch) > -1;
-    const textSearchResultOfProject = jQuery(this).find('.gnome-project-name').text().toLowerCase().indexOf(
-      currentProjectSearch) > -1;
+    filteredGnomeProjects.filter((project) => {
+        const jqueryProject = jQuery(project);
 
-    const currentIndex = parseInt(jQuery(this).attr('data-index-project'));
+        const textSearchResultOfMember = jqueryProject.find('.gnome-member-name').text().toLowerCase().indexOf(
+            currentProjectSearch) > -1;
 
-    const maximumPageNumberResult = currentIndex <= currentProjectPage * amountOfProjectsPerPage;
+        const textSearchResultOfProject = jqueryProject.find('.gnome-project-name').text().toLowerCase().indexOf(
+            currentProjectSearch) > -1;
 
-    const minimalPageNumberResult = currentProjectPage > 1 ? currentIndex > ((currentProjectPage - 1) * amountOfProjectsPerPage) : true;
+        const currentIndex = parseInt(jqueryProject.attr('data-index-project'));
 
-    jQuery(this).toggle((textSearchResultOfMember || textSearchResultOfProject) && maximumPageNumberResult &&
-      minimalPageNumberResult);
-  })
+        const maximumPageNumberResult = currentIndex <= currentProjectPage * amountOfProjectsPerPage;
+
+        const minimalPageNumberResult = currentProjectPage > 1 ? currentIndex > ((currentProjectPage - 1) * amountOfProjectsPerPage) : true;
+
+        jqueryProject.toggle((textSearchResultOfMember || textSearchResultOfProject) && maximumPageNumberResult &&
+            minimalPageNumberResult);
+    })
 }
 
 const showProjectPage = (pagination) => {
-  const element = jQuery(pagination);
+    const element = jQuery(pagination);
 
-  currentProjectPage = parseInt(element.text());
+    currentProjectPage = parseInt(element.text());
 
-  filterProjectMethod();
+    filterProjectMethod();
 
-  jQuery('.page-item').removeClass('active');
-  element.addClass('active');
+    jQuery('.page-item').removeClass('active');
+
+    element.addClass('active');
 };
 
+const hideProjectsWithoutMaintainers = () => {
+    gnomeProjects.forEach((project) => {
+        const membersContainer = project.querySelector(".project-members");
+
+        if (membersContainer.children.length === 0) {
+            project.classList.add('d-none');
+        }
+    })
+}
+
+const paginationProject = jQuery('.pagination-project');
+
 for (let i = 1; i <= amountOfProjectsPages; i++) {
-  jQuery('.pagination-project').
-  append(
-    `<li class="page-item" style="cursor: pointer" onClick="showProjectPage(this)"><span class="page-link">${i}</span></li>`
+    paginationProject.append(
+        `<li class="page-item" style="cursor: pointer" onClick="showProjectPage(this)"><span class="page-link">${i}</span></li>`
     );
 }
 
 jQuery('.gnome-projects-filter').on('keyup', function () {
-  currentProjectSearch = jQuery(this).val().toLowerCase();
+    currentProjectSearch = jQuery(this).val().toLowerCase();
 
-  filterProjectMethod();
+    filterProjectMethod();
 });
 
 jQuery('.page-item').first().addClass('active');
 
 filterProjectMethod();
+
+hideProjectsWithoutMaintainers();
