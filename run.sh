33779c5b324acfcedba577975ef1a332ab15a165
Claudio W <cwunder@gnome.org>
2022-03-12 08:32:39 +0000

    fix(rbenv): use globally

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ BUNDLER_PATH=$GEM_PATH/bin
 eval "$(rbenv init -)"
 
 # set the ruby version locally to 2.7.5
-rbenv local 2.7.5
+rbenv global 2.7.5
 
 echo -e "\n💡 \033[1;31mplease access via localhost and not 127.0.0.1! \033[0m"
 
