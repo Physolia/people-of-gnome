3180156c46f3fb04c4f244913281ac454a4d07e9
Asmit Malakannawar <asmitbm2952002@gmail.com>
2022-03-18 21:41:32 +0530

    fix: Scroll to top button

--- assets/js/go-to-top.js
+++ assets/js/go-to-top.js
@@ -3,21 +3,21 @@ const scrollToTop = document.getElementById('btn-go-to-top')
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
-scrollToTop.addEventListener('click', function (e) {
+scrollToTop.addEventListener('click', (e) => {
     e.preventDefault();
 
-    window.scroll({
+    scroll({
         top: 0,
         left: 0,
         behavior: 'smooth'
     })
 });
 
-document.addEventListener('scroll', function (e) {
+document.addEventListener('scroll', () => {
     lastKnownScrollPosition = window.scrollY;
     
     if (!ticking) {
-        window.requestAnimationFrame(function() {
+        requestAnimationFrame(() => {
             if (lastKnownScrollPosition < 400) {
               scrollToTop.classList.add('d-none')
             }
