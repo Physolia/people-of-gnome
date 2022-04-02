52a8bed0932bf4161bd693a341ab99f4c8ae2b64
Asmit Malakannawar <asmitbm2952002@gmail.com>
2022-04-02 21:48:52 +0530

    fix: run prettier on this file

--- assets/js/go-to-top.js
+++ assets/js/go-to-top.js
@@ -1,4 +1,4 @@
-const scrollToTop = document.getElementById('btn-go-to-top')
+const scrollToTop = document.getElementById('btn-go-to-top');
 
 let lastKnownScrollPosition = 0;
 let ticking = false;
@@ -9,25 +9,27 @@ scrollToTop.addEventListener('click', (e) => {
     scroll({
         top: 0,
         left: 0,
-        behavior: 'smooth'
-    })
+        behavior: 'smooth',
+    });
 });
 
-document.addEventListener('scroll', () => {
-    lastKnownScrollPosition = window.scrollY;
-    
-    if (!ticking) {
-        requestAnimationFrame(() => {
-            if (lastKnownScrollPosition < 400) {
-              scrollToTop.classList.add('d-none')
-            }
-            else {
-              scrollToTop.classList.remove('d-none')
-            }
-            ticking = false;
-        });
-        
-        ticking = true;
-    }
-}, 
-{ passive: true });
+document.addEventListener(
+    'scroll',
+    () => {
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+            requestAnimationFrame(() => {
+                if (lastKnownScrollPosition < 400) {
+                    scrollToTop.classList.add('d-none');
+                } else {
+                    scrollToTop.classList.remove('d-none');
+                }
+                ticking = false;
+            });
+
+            ticking = true;
+        }
+    },
+    { passive: true }
+);
